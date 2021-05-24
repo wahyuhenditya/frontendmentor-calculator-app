e3513f5da6f562e22b453f6090d04b2afec328dc
Wahyu Henditya <wahyuhandy1993@gmail.com>
2021-05-24 21:04:38 +0700

    linter setup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import React, { useState } from "react";
-import data from "buttonData";
-import classnames from "classnames";
+import React from 'react';
+import classnames from 'classnames';
+import data from 'buttonData';
 
 function App() {
-	const populateButton = () => {
-		return data.map((dt, idx) => (
-			<div className={classnames("button", dt.type)}>
-				<span>{dt.text}</span>
-			</div>
-		));
-	};
+  const populateButton = () =>
+    data.map((dt) => (
+      <div key={dt.text} className={classnames('button', dt.type)}>
+        <span>{dt.text}</span>
+      </div>
+    ));
 
-	const changeTheme = (e, number) => {
-		const bodyElement = document.querySelector("body");
-		const indicatorElement = document.querySelectorAll(".indicator");
-		// Change active
-		indicatorElement.forEach((element) => {
-			element.classList.remove("active");
-		});
-		e.target.classList.add("active");
+  const changeTheme = (e, number) => {
+    const bodyElement = document.querySelector('body');
+    const indicatorElement = document.querySelectorAll('.indicator');
 
-		// Inject theme to body
-		bodyElement.className = "";
-		bodyElement.classList.add("theme" + number);
-	};
+    // Change active
+    indicatorElement.forEach((element) => {
+      element.classList.remove('active');
+    });
+    e.target.classList.add('active');
 
-	return (
-		<div className="App">
-			<div className="top">
-				<div className="title">calc</div>
-				<div className="theme">
-					<span>Theme</span>
-					<div className="select">
-						<div className="indicator active" onClick={(e) => changeTheme(e, 1)}></div>
-						<div className="indicator" onClick={(e) => changeTheme(e, 2)}></div>
-						<div className="indicator" onClick={(e) => changeTheme(e, 3)}></div>
-					</div>
-				</div>
-			</div>
-			<div className="screen">
-				<span>399,981</span>
-			</div>
-			<div className="operation">{populateButton()}</div>
-		</div>
-	);
+    // Inject theme to body
+    bodyElement.className = '';
+    bodyElement.classList.add(`theme${number}`);
+  };
+
+  return (
+    <div className="App">
+      <div className="top">
+        <div className="title">calc</div>
+        <div className="theme">
+          <span>Theme</span>
+          <div className="select">
+            <div
+              className="indicator active"
+              onClick={(e) => changeTheme(e, 1)}
+              role="button"
+              aria-label="theme1"
+              tabIndex={0}
+            />
+            <div
+              className="indicator"
+              onClick={(e) => changeTheme(e, 2)}
+              role="button"
+              aria-label="theme1"
+              tabIndex={0}
+            />
+            <div
+              className="indicator"
+              onClick={(e) => changeTheme(e, 3)}
+              role="button"
+              aria-label="theme1"
+              tabIndex={0}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="screen">
+        <span>399,981</span>
+      </div>
+      <div className="operation">{populateButton()}</div>
+    </div>
+  );
 }
 
 export default App;
